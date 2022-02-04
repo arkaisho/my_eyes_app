c9e97c41970053abdc3f92234dc2e1533e6a2fbd
Joabson <joabson.nascimento.117@ufrn.edu.br>
2022-02-04 18:15:44 -0300

    performs visual refactoring of product listing and detail screens and profile screen

--- lib/app/modules/profile/pages/profile_page.dart
+++ lib/app/modules/profile/pages/profile_page.dart
@@ -1,15 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:my_eyes/app/modules/profile/profile_store.dart';
-import 'package:flutter/material.dart';
 import 'package:my_eyes/app/shareds/custom_bottom_navigation_bar.dart';
 import 'package:my_eyes/app/shareds/custom_colors.dart';
 import 'package:my_eyes/app/utils/authentication.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfilePage extends StatefulWidget {
   final String title;
+
   const ProfilePage({Key? key, this.title = 'ProfilePage'}) : super(key: key);
+
   @override
   ProfilePageState createState() => ProfilePageState();
 }
@@ -17,12 +18,14 @@ class ProfilePage extends StatefulWidget {
 class ProfilePageState extends State<ProfilePage> {
   final ProfileStore store = Modular.get();
   String name = "...";
+  String email = "...";
 
   @override
   void initState() {
     store.me().then((response) {
       setState(() {
         this.name = response['username'].toString();
+        this.email = response['email'].toString();
       });
     });
 
@@ -33,114 +36,255 @@ class ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.white,
         elevation: 0,
         title: Text(
           "Perfil",
           style: GoogleFonts.raleway(
-            fontWeight: FontWeight.bold,
-            color: CustomColors.mainBlue,
-            fontSize: 20,
+            color: Color(0xFF090F13),
+            fontSize: 28,
+            fontWeight: FontWeight.w500,
           ),
         ),
         automaticallyImplyLeading: false,
       ),
       bottomNavigationBar: CustomBottomNavigationBar(),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        child: Stack(
-          children: [
-            SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      backgroundColor: Color(0xFFF1F4F8),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(24, 12, 24, 12),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Container(
+                        width: 76,
+                        height: 76,
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                        ),
+                        child: CircleAvatar(
+                          backgroundColor: CustomColors.mainBlue,
+                          radius: 60,
+                          child: Icon(
+                            Icons.person_outline,
+                            color: Colors.white,
+                            size: 50,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(16, 0, 0, 0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                              child: Text(
+                                name,
+                                style: GoogleFonts.raleway(
+                                  color: Color(0xFF090F13),
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                              child: Text(
+                                email,
+                                style: GoogleFonts.raleway(
+                                  color: Color(0xFF57636C),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(24, 12, 0, 12),
+                    child: Text(
+                      'Configurações da Conta',
+                      style: GoogleFonts.raleway(
+                        color: Color(0xFF090F13),
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                child: Ink(
+                  width: double.infinity,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 5,
+                        color: Color(0x3416202A),
+                        offset: Offset(0, 2),
+                      )
+                    ],
+                    borderRadius: BorderRadius.circular(12),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: InkWell(
+                    onTap: () {
+                      Modular.to.pushNamed("/home/login/recover_password");
+                    },
+                    child: Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(20, 0, 0, 0),
+                            child: Text(
+                              'Mudar senha',
+                              style: GoogleFonts.raleway(
+                                color: Color(0xFF57636C),
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Align(
+                              alignment: AlignmentDirectional(0.9, 0),
+                              child: Icon(
+                                Icons.arrow_forward_ios,
+                                color: Color(0xFF95A1AC),
+                                size: 18,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(20, 12, 20, 0),
+                child: Container(
+                  width: double.infinity,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 5,
+                        color: Color(0x3416202A),
+                        offset: Offset(0, 2),
+                      )
+                    ],
+                    borderRadius: BorderRadius.circular(12),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(20, 0, 0, 0),
+                          child: Text(
+                            'Termos e Serviços',
+                            style: GoogleFonts.raleway(
+                              color: Color(0xFF57636C),
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Align(
+                            alignment: AlignmentDirectional(0.9, 0),
+                            child: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Color(0xFF95A1AC),
+                              size: 18,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 20),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Center(
-                      child: CircleAvatar(
-                        backgroundColor: CustomColors.mainBlue,
-                        radius: 60,
-                        child: Icon(
-                          Icons.person_outline,
-                          color: Colors.white,
-                          size: 50,
-                        ),
-                      ),
-                    ),
-                    Container(height: 20),
-                    Center(
-                      child: Text(
-                        name,
-                        style: GoogleFonts.raleway(
-                          color: CustomColors.mainBlue,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                    Container(height: 100),
-                    ProfileOptionTile(
-                      onTap: () {
+                    ElevatedButton.icon(
+                      onPressed: () {
                         Authentication.logout();
                         Modular.to.pushReplacementNamed("/home");
                       },
-                      text: "Sair",
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      label: Text(
+                        "Sair",
+                        style: GoogleFonts.raleway(
+                          color: Colors.black,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                       icon: Icon(
                         Icons.door_back_door_outlined,
-                        color: CustomColors.mainBlue,
+                        color: Colors.black,
                       ),
                     ),
-                    Container(height: 50),
                   ],
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ProfileOptionTile extends StatelessWidget {
-  final Function() onTap;
-  final String text;
-  final Widget icon;
-  const ProfileOptionTile({
-    required this.onTap,
-    required this.icon,
-    required this.text,
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        height: 40,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              text,
-              style: GoogleFonts.raleway(
-                color: CustomColors.mainBlue,
-                fontWeight: FontWeight.bold,
-                fontSize: 14,
-              ),
-            ),
-            icon
-          ],
-        ),
-        decoration: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(
-              width: 1,
-              color: Colors.black.withOpacity(.3),
-            ),
+            ],
           ),
-        ),
+        ],
       ),
     );
   }
