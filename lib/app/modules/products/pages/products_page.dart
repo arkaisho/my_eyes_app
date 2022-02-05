e83a8a04e910073585a0f39db331ad2cdc02e8fd
joao <joaopedro10782@gmail.com>
2022-02-05 10:39:52 -0300

    visual adjustments

--- lib/app/modules/products/pages/products_page.dart
+++ lib/app/modules/products/pages/products_page.dart
@@ -17,10 +17,14 @@ class ProductsPageState extends State<ProductsPage> {
   final ProductsStore store = Modular.get();
 
   int currentIndex = 0;
+  bool _didFirstLoading = false;
 
   @override
-  void didChangeDependencies() {
-    store.products(context);
+  void didChangeDependencies() async {
+    await store.products(context);
+    setState(() {
+      _didFirstLoading = true;
+    });
     super.didChangeDependencies();
   }
 
@@ -60,60 +64,73 @@ class ProductsPageState extends State<ProductsPage> {
             children: [
               Padding(
                 padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 12),
-                child: store.productList.length > 0 ? ListView(
-                  physics: const AlwaysScrollableScrollPhysics(),
-                  children: store.productList
-                      .map((product) => ProductTile(product: product))
-                      .toList(),
-                ) : Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.fastfood_outlined,
-                      color: Color(0xFF8B97A2),
-                      size: 90,
-                    ),
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Você não possui nenhum produto',
-                            textAlign: TextAlign.center,
-                            style: GoogleFonts.raleway(
-                              color: Color(0xFF090F13),
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(12, 4, 12, 0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              'Adicione novos produtos e eles aparecerão aqui.',
-                              textAlign: TextAlign.center,
-                              style: GoogleFonts.raleway(
+                child: store.productList.length > 0
+                    ? ListView(
+                        physics: const AlwaysScrollableScrollPhysics(),
+                        children: store.productList
+                            .map((product) => ProductTile(product: product))
+                            .toList(),
+                      )
+                    : _didFirstLoading
+                        ? Column(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.fastfood_outlined,
                                 color: Color(0xFF8B97A2),
-                                fontSize: 14,
-                                fontWeight: FontWeight.normal,
+                                size: 90,
+                              ),
+                              Padding(
+                                padding:
+                                    EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      'Você não possui nenhum produto',
+                                      textAlign: TextAlign.center,
+                                      style: GoogleFonts.raleway(
+                                        color: Color(0xFF090F13),
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(
+                                    12, 4, 12, 0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        'Adicione novos produtos e eles aparecerão aqui.',
+                                        textAlign: TextAlign.center,
+                                        style: GoogleFonts.raleway(
+                                          color: Color(0xFF8B97A2),
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          )
+                        : Center(
+                            child: Container(
+                              width: 30,
+                              child: LinearProgressIndicator(
+                                color: CustomColors.mainBlue,
                               ),
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
               ),
               Positioned(
                 bottom: 0,
